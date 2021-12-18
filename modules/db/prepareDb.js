0001dced0cf8a2a27d2290e159ba854c7a45dd81
RedCrafter07 <owner@redcrafter07.de>
2021-12-18 15:28:20 +0100

    more db preperation

--- modules/db/prepareDb.js
+++ modules/db/prepareDb.js
@@ -1,11 +1,15 @@
 const chalk = require('chalk');
 
-module.exports = async function(db) {
-	let data = await db.getData('/');
-	if (!data.isSetuped) {
-		await db.push('/isSetuped', false);
-		console.log(chalk.red("[DATABASE]: Login is not setuped. Added 'isSetuped: false' to database."));
-	}
+module.exports = async db =>
+	new Promise(async resolve => {
+		let data = await db.getData('/');
+		if (!data.isSetuped && data.isSetuped != false && data.isSetuped != true) {
+			await db.push('/isSetuped', false);
+			await db.push('/setupStep', 0);
+			console.log(chalk.red("[DATABASE]: Login is not setuped. Added 'isSetuped: false' to database."));
+		}
 
-	console.log(chalk.green('[DATABASE]: Database prepared.'));
-};
+		console.log(chalk.green('[DATABASE]: Database prepared.'));
+
+		resolve();
+	});
